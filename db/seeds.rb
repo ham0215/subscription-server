a8db7adfaddc5468b90e1d95ae8ab3ada379fec2
Naoto Hamada <naoto.hamada@findy.co.jp>
2022-11-04 13:52:01 +0900

    Fix seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(name: 'ham')
+User.insert_all((1..100).map { { name: "ham#{_1}" } })
